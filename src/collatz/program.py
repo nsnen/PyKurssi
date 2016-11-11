707dc9c7d151a9f90936d8b7c0f43a4321d09760
nsnen <niina.snen@gmail.com>
2016-11-11 16:55:27 +0200

    app 2: guess number

---
+++ src/collatz/program.py
@@ -0,0 +1,24 @@
+print('------------------------')
+print('     COLLATZ APP')
+print('------------------------')
+print('')
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number//2)
+        return number//2
+    elif number % 2 == 1:
+        result = 3 * number + 1
+        print(result)
+        return result
+
+n = input('enter a number: ')
+while n != 1:
+    n = collatz(int(n))
+
+print('')
+print('the end.')
+
+
+
+
